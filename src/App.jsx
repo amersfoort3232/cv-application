77f1d2b32aae147ca9d6a3c40a0dedf7b285277c
amersfoort3232 <amersfoortlam@gmail.com>
2024-03-01 15:58:38 +0000

    added basic react.js code
    completed the logic of the html.
    the experience and education work now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
 import { useState } from "react";
-
-const educationInfo = [];
+import "./style.css";
 
 function App() {
+  const [educationItems, setEducationItems] = useState([]);
+  const [personalInfo, setPersonalInfo] = useState({});
+  const [experienceItems, setExperienceItems] = useState([]);
+
+  function handleAddEducation(educationItem) {
+    setEducationItems((educationItems) => [...educationItems, educationItem]);
+  }
+
+  function handleAddPersonalInfo(personalInfo) {
+    setPersonalInfo(personalInfo);
+  }
+  function handleAddExperience(experienceItem) {
+    setExperienceItems((experienceItems) => [
+      ...experienceItems,
+      experienceItem,
+    ]);
+  }
+
   return (
-    <div className="inputContainer">
-      <div className="generalInfo">
-        <h1>CV Application</h1>
+    <main className="mainContainer">
+      <div className="inputContainer">
+        <GeneralInfo onAddPersonalInfo={handleAddPersonalInfo} />
+        <Education onAddEducation={handleAddEducation} />
+        <Experience onAddExperience={handleAddExperience} />
       </div>
-      <GeneralInfo />
-      <Education />
-      <Experience />
-    </div>
+      <div className="outputContainer">
+        <div className="personalInfo">
+          <DisplayName personalInfo={personalInfo} />
+        </div>
+        <div className="educationList">
+          <EducationList educationItems={educationItems} />
+        </div>
+        <div className="experienceList">
+          <ExperienceList experienceItems={experienceItems} />
+        </div>
+      </div>
+    </main>
   );
 }
 
-function handleSubmit(e) {
+/* function handleSubmit(e) {
   e.preventDefault();
   const form = e.target;
   console.log("Form submitted");
@@ -23,26 +50,68 @@ function handleSubmit(e) {
 
   const formJson = Object.fromEntries(formData.entries());
   console.log(formJson);
-}
+} */
 
-function GeneralInfo() {
+function GeneralInfo({ onAddPersonalInfo }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
+
+  function handlePersonalInfoSubmit(e) {
+    e.preventDefault();
+    console.log("Personal Information added");
+
+    const newPersonalInfo = {
+      name,
+      email,
+      tel,
+    };
+
+    onAddPersonalInfo(newPersonalInfo);
+
+    setName(name);
+    setEmail(email);
+    setTel(tel);
+  }
+
   return (
     <>
       <h1>General Info</h1>
-      <form method="post" onSubmit={handleSubmit}>
+      <form method="post" onSubmit={handlePersonalInfoSubmit}>
         <label htmlFor="name">
           First name:
-          <input type="text" id="name" name="name" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <br />
         <label htmlFor="email">
           Email:
-          <input type="email" id="email" name="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <br />
         <label htmlFor="tel">
           Phone number:
-          <input type="tel" id="tel" name="tel" />
+          <input
+            type="tel"
+            id="tel"
+            name="tel"
+            value={tel}
+            required
+            onChange={(e) => setTel(e.target.value)}
+          />
         </label>
         <br />
         <button type="submit">Submit</button>
@@ -51,8 +120,7 @@ function GeneralInfo() {
   );
 }
 
-function Education() {
-  const [educations, setEducation] = useState(educationInfo);
+function Education({ onAddEducation }) {
   const [school, setSchool] = useState("");
   const [title, setTitle] = useState("");
   const [startDate, setStartDate] = useState("");
@@ -69,15 +137,12 @@ function Education() {
       endDate,
     };
 
-    console.log(newEducationInfo);
-
-    setEducation([...educations, newEducationInfo]);
+    onAddEducation(newEducationInfo);
 
     setSchool("");
     setTitle("");
     setStartDate("");
     setEndDate("");
-    console.log(educations);
   }
 
   /*  const addEducation = ({ school, title, startDate, endDate }) => {
@@ -137,27 +202,145 @@ function Education() {
   );
 }
 
-function Experience() {
+function EducationList({ educationItems }) {
+  return (
+    <div className="education">
+      <h1>Education</h1>
+      <ul>
+        {educationItems.map((education) => (
+          <EducationItem educationItem={education} key={education.startDate} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function EducationItem({ educationItem }) {
+  return (
+    <li>
+      <h3>{educationItem.title}</h3>
+      {educationItem.school}
+      <p>
+        {educationItem.startDate} - {educationItem.endDate}
+      </p>
+    </li>
+  );
+}
+
+function ExperienceList({ experienceItems }) {
+  return (
+    <div className="education">
+      <h1>Experience</h1>
+      <ul>
+        {experienceItems.map((experience) => (
+          <ExperienceItem
+            experienceItem={experience}
+            key={experience.startDate}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function ExperienceItem({ experienceItem }) {
+  return (
+    <li>
+      <h3>{experienceItem.position}</h3>
+      <h4>{experienceItem.company}</h4>
+      <p>
+        {experienceItem.startDate} - {experienceItem.endDate}
+      </p>
+      <p>{experienceItem.description}</p>
+    </li>
+  );
+}
+function Experience({ onAddExperience }) {
+  const [company, setCompany] = useState("");
+  const [position, setPosition] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handleExperienceSubmit(e) {
+    e.preventDefault();
+    console.log("Experience Information added");
+
+    const newExperienceInfo = {
+      company,
+      position,
+      startDate,
+      endDate,
+      description,
+    };
+
+    onAddExperience(newExperienceInfo);
+
+    setCompany("");
+    setPosition("");
+    setStartDate("");
+    setEndDate("");
+    setDescription("");
+  }
+
   return (
     <>
       <h1>Experience</h1>
-      <form>
+      <form onSubmit={handleExperienceSubmit}>
         <label htmlFor="company">Company: </label>
-        <input type="text" id="company" name="company" />
+        <input
+          type="text"
+          id="company"
+          name="company"
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+        />
         <br />
         <label htmlFor="position">Position: </label>
-        <input type="text" id="company" name="position" />
+        <input
+          type="text"
+          id="company"
+          name="position"
+          value={position}
+          onChange={(e) => setPosition(e.target.value)}
+        />
         <br />
         <label htmlFor="startDate">Start Date: </label>
-        <input type="month" id="startDate" name="startDate" />
+        <input
+          type="month"
+          id="startDate"
+          name="startDate"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
         <br />
         <label htmlFor="endDate">End Date: </label>
-        <input type="month" id="endDate" name="endDate" />
+        <input
+          type="month"
+          id="endDate"
+          name="endDate"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
         <br />
         <label htmlFor="description">Description: </label>
-        <textarea id="description" name="description"></textarea>
+        <textarea
+          id="description"
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
         <button type="submit">Submit</button>
       </form>
+    </>
+  );
+}
+
+function DisplayName({ personalInfo }) {
+  return (
+    <>
+      <h1>Personal Infomation</h1>
+      <h2>{personalInfo.name}</h2>
+      <h4>{personalInfo.email}</h4> <h4>{personalInfo.tel}</h4>
     </>
   );
 }
